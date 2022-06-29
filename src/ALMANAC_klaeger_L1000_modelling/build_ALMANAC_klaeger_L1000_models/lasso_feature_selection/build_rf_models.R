77b331bc5008cf802242fa4df53c129c502f8c07
cujoisa <cujoisa@live.unc.edu>
2022-06-29 16:15:03 -0400

    adding code to assess models

--- src/ALMANAC_klaeger_L1000_modelling/build_ALMANAC_klaeger_L1000_models/lasso_feature_selection/build_rf_models.R
+++ src/ALMANAC_klaeger_L1000_modelling/build_ALMANAC_klaeger_L1000_models/lasso_feature_selection/build_rf_models.R
@@ -8,20 +8,24 @@ library(doParallel)
 library(patchwork)
 library(ROCR)
 library(argparse)
-library(xgboost)
+library(ranger)
+library(conflicted)
+conflict_prefer("slice", "dplyr")
+
+tic()
+
+
+dir.create(here('results/ALMANAC_klaeger_L1000_models/activation_expression/regression/', 
+                sprintf('random_forest/results')), 
+           showWarnings = F, recursive = T)
+
+full_output_file = here('results/ALMANAC_klaeger_L1000_models/activation_expression/regression/random_forest/results/lasso_feature_results_rf.rds.gz')
 
 full_dataset = read_rds(here('results/ALMANAC_klaeger_L1000_data_for_ml.rds.gz'))
 feats =  vroom(here('results/ALMANAC_klaeger_L1000_models/feature_selection/lasso_selected_features.csv'))
+rf_grid = read_rds(here('results/hyperparameter_grids/rf_grid.rds'))
 set.seed(2222)
 folds = vfold_cv(full_dataset, v = 5)
-
-tic()	
-
-dir.create(here('results/ALMANAC_klaeger_L1000_models/activation_expression/regression/', 
-                sprintf('xgboost/results')), 
-           showWarnings = F, recursive = T)
-
-full_output_file = here('results/ALMANAC_klaeger_L1000_models/activation_expression/regression/xgboost/results/lasso_feature_results_inital.rds.gz')
 
 id_vars = full_dataset %>% 
   select(-starts_with(c("act_", "pert_")), -viability)
@@ -32,20 +36,15 @@ this_recipe = recipe(viability ~ ., full_dataset) %>%
   step_select(viability, any_of(c(names(id_vars), feats$feature))) %>% 
   step_zv(all_predictors())
 
-xgb_spec <- boost_tree(
-  trees = tune(), 
-  tree_depth = tune()
-) %>% 
-  set_engine("xgboost", tree_method = "gpu_hist") %>% 
+rf_spec <- rand_forest(
+  trees = tune()
+) %>% set_engine("ranger", num.threads = 16) %>%
   set_mode("regression")
-
-set.seed(2222)
-xgb_grid = read_rds(here('results/hyperparameter_grids/xgb_grid.rds'))
 
 this_wflow <-
   workflow() %>%
-  add_model(xgb_spec) %>%
-  add_recipe(this_recipe)
+  add_model(rf_spec) %>%
+  add_recipe(this_recipe) 
 
 race_ctrl = control_grid(
   save_pred = TRUE, 
@@ -56,10 +55,10 @@ race_ctrl = control_grid(
 results <- tune_grid(
   this_wflow,
   resamples = folds,
-  grid = xgb_grid,
+  grid = rf_grid,
   control = race_ctrl
 ) %>% 
-  #collect_metrics() %>% 
+  collect_metrics() %>% 
   write_rds(full_output_file, compress = "gz")
 
 toc()
